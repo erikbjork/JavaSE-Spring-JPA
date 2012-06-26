7b8dcdbad4dae2c465b8230e5cccfcd109c5e2ae
Erik Björk <erik@ebjork.se>
2012-06-26 17:32:02 +0200

    Initial commit with a simple Java SE application.

---
+++ src/main/java/se/ebjork/app/App.java
@@ -0,0 +1,15 @@
+package se.ebjork.app;
+
+public class App {
+
+    private App() {
+
+    }
+
+    public static void main(final String[] args) {
+
+        System.out.println("Hello World!");
+
+    }
+
+}
