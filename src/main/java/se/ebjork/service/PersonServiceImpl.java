8392bd8c0291ee80e102267230634140172ab2dd
Erik Björk <erik@ebjork.se>
2012-06-27 07:11:54 +0200

    Added logging components SLF4J and Log4J.

--- src/main/java/se/ebjork/service/PersonServiceImpl.java
+++ src/main/java/se/ebjork/service/PersonServiceImpl.java
@@ -3,6 +3,8 @@ package se.ebjork.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,21 +19,28 @@ import se.ebjork.domain.Person;
 @Transactional
 public class PersonServiceImpl implements PersonService {
 
+    private static Logger logger = LoggerFactory.getLogger(PersonServiceImpl.class);
+
     @Autowired
     private PersonDao personDao;
 
     @Override
     public final void addPerson(final String firstName, final String lastName) {
+        logger.debug("Adding a person.");
+
         final Person person = new Person();
         person.setFirstName(firstName);
         person.setLastName(lastName);
 
         personDao.save(person);
+        logger.info("Added person: {} {}", firstName, lastName);
     }
 
     @Override
     @Transactional(readOnly = true)
     public final List<String> listPersons() {
+        logger.debug("Listing persons.");
+
         final List<Person> personList = personDao.findAll();
 
         final List<String> result = new ArrayList<String>();
